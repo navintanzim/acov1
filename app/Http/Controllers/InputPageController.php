79972f57c95d027b4bf38d282500231e71e466ec
JIRA Developer <tanzim@ba-systems.com>
2024-10-19 22:07:46 +0600

    First commit

---
+++ app/Http/Controllers/InputPageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InputPageController extends Controller
+{
+    public function inputpage()
+    {
+        
+        return view('input');
+    }
+}
